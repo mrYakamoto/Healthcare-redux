541ee92d1c2bbf741ac91d66231d6ef2e05afba3
Gregory Wehmeier <wehmenstein@gmail.com>
2016-05-12 17:00:25 -0500

    add clinic routes, migration, and model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def clinics
+    if request.xhr?
+      if (params[:type] == "FP")
+        @clinics = Clinic.where(:clinic_type => ["FP", "PP"])
+      else
+        @clinics = Clinic.where(clinic_type: params[:type])
+      end
+      render :json => @clinics
+    end
+  end
+
+  def geolocate
+    if request.xhr?
+      @clinics = Clinic.near(params[:location], 100)
+      render :json => @clinics
+    end
+  end
 end
